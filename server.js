1c25ddb09d798b6466b168c852f3579e070d189f
CV <brunchwithyou@gmail.com>
2023-01-26 22:35:45 -0800

    Added image, styled background, added starting js and editted interactions

--- server.js
+++ server.js
@@ -34,5 +34,5 @@ app.get('/', async (request, response) => {
 
 //PORT = 8050
 app.listen(process.env.PORT || PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+    console.log(`Server running on port ${process.env.PORT || PORT}`)
 })
