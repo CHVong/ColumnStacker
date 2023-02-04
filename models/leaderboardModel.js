4e3ebdc9c5966e3ec3c2a2d64273496d8d0b8c32
CV <brunchwithyou@gmail.com>
2023-02-04 13:37:06 -0800

    Added placeholder for name submission, testing out dependencies for date

--- models/leaderboardModel.js
+++ models/leaderboardModel.js
@@ -1,4 +1,10 @@
 const mongoose = require('mongoose');
+const moment = require('moment');
+const momentTz = require('moment-timezone');
+
+moment.tz = momentTz.tz;
+
+
 const leaderboardSchema = new mongoose.Schema({
 userName: {
     type: String,
@@ -7,6 +13,12 @@ userName: {
 score: {
     type: Number,
     required: true
+},
+date: {
+    type: String,
+    default: function() {
+        return moment().tz('America/Los_Angeles').format('MM/DD/YYYY h:mm A');
+    }
 }
 })
 
